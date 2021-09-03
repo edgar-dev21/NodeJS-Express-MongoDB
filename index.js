75ffd32dd3c156ad9fd961846de3ec41e2478b57
Edgar Gonzalez <edgar.gonzalez@utexas.edu>
2021-09-03 13:45:50 -0500

    Mongoose Part 2

--- index.js
+++ index.js
@@ -8,16 +8,15 @@ const connect = mongoose.connect(url, {
     useUnifiedTopology: true
 });
 
+
 connect.then(() => {
 
     console.log('Connected correctly to server');
 
-    const newCampsite = new Campsite({
+    Campsite.create({
         name: 'React Lake Campground',
         description: 'test'
-    });
-
-    newCampsite.save()
+    })
     .then(campsite => {
         console.log(campsite);
         return Campsite.find();
